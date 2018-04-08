2520330421d942110e659359d9a89639ec9138a7
Andy Welton <awelton1@gmail.com>
2018-04-07 21:17:14 -0500

    reorganizing without folder structures

---
+++ convert_heic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+prefix='./'
+suffix='.heic'
+slash='/'
+
+inpath="${1%$slash}/"
+
+for file in ${inpath}*
+do
+    ((i=i%${2})); ((i++==0)) && wait
+    fileout=${file#$prefix}
+    fileout=${fileout%$suffix}
+    fileout="${fileout}.jpg"
+    #echo "Converting ${file} to ${fileout}"
+    `tifig $file $fileout -q=100` &
+done
+
+`mkdir ${inpath}./JPEG`
+`mv ${inpath}*.jpg ${inpath}./JPEG`
